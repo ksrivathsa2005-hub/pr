b0917677e3f98eb24803770ce705d5f60184503e
P Sai Praneeth Kumar <praneeth@nalashaa.net>
2026-01-28 09:50:10 +0530

    Initial commit

---
+++ test.ts
@@ -0,0 +1,9 @@
+let a: number =10;
+let b: number =20;
+console.log(a*b);
+
+var username:string = "praneeth";
+var age:number = 21;
+var isActive:boolean = true;
+
+console.log(username,age,isActive);
